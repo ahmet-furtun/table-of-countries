1e5bb00fbb35c617a176af15f1d687b4a0299868
Ahmet FURTUN <ahmet.furtun1@gmail.com>
2021-02-20 22:45:30 +0300

    First commit

---
+++ src/components/app/app.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app.component.css';
+import '../../common/styles';
+
+const App = () => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+
+export default App;
